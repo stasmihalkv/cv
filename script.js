d32ee718a47e0ad86dcd0bf950b73a68bc5f63e1
Stanislav <39649166+stasmihalkv@users.noreply.github.com>
2021-01-02 20:38:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 window.onload = function(){
-    var changeBg = document.querySelector('.changeBgIcon');
-    var all = document.querySelector('body');
-    var projectsItem = document.querySelectorAll('.main-block__projects-item');
-    var profilePhoto = document.querySelector('.aside-content__profile-photo')
-    profilePhoto.classList.add('profile-photo_active')
+    let changeBg = document.querySelector('.changeBgIcon');
+    let mainBg = document.querySelector('body');
+    let projectsItem = document.querySelectorAll('.main-block__projects-item');
+    let profilePhoto = document.querySelector('.aside-content__profile-photo');
+    let asideWrapper = document.querySelector('.aside-wrapper');
+    profilePhoto.classList.add('profile-photo_active')  
+    
 
     for(i=0;i<projectsItem.length;i++){
         projectsItem[i].onmouseover = handlerMouseBig;
-        projectsItem[i].onmouseout = handlerMouseSmall;  
-        changeBg.onclick = activateDarkTheme;
-
+        projectsItem[i].touchstart = handlerMouseBig;
+        projectsItem[i].onmouseout = handlerMouseSmall; 
+        projectsItem[i].touchend = handlerMouseSmall; 
+        changeBg.onclick = activateDarkMode;
+         
     }
-    function handlerMouseBig(){ 
+
+    function handlerMouseBig(){      
         this.classList.add('projects-item_active')
         for(i=0;i<projectsItem.length;i++){
             if(!projectsItem[i].classList.contains('projects-item_active')){
                projectsItem[i].classList.add('projects-item_non-active')
-            }  
+            } 
+            
         }
     }
     function handlerMouseSmall(){ 
-        this.classList.remove('projects-item_active')
-        for(i=0;i<projectsItem.length;i++){
+        this.classList.remove('projects-item_active')      
+        for(i=0;i<projectsItem.length;i++){        
             if(!projectsItem[i].classList.contains('projects-item_active')){
             projectsItem[i].classList.remove('projects-item_non-active')
             }  
         }
 }
-    function activateDarkTheme(){
-        all.classList.toggle('dark_active');
+    function activateDarkMode(){
+        mainBg.classList.toggle('dark_active');
+        asideWrapper.classList.toggle('aside-wrapper_active')
         this.classList.toggle('dark_active');
         for(i=0;i<projectsItem.length;i++){
             projectsItem[i].classList.toggle('dark_active');
         }
     };
 
-
-
 }
 
-
-    /*   $(".projects_item").hover(function(){
-    $(this).animate({opacity:'1'})},
-        function(){$(this).stop().animate({opacity:'0.3'})
-    }
-    );*/
-
-    /* projectsItem.width(90).hover(function(){
-        $(this).stop().animate({width: '900' +'px'});
-    },
-    function(){
-        $(this).stop().animate({width: 90});
-    });*/
+/*
+    if (screen.width > 640) {
+        $(".changeBgIcon").animate(
+            {"right": "60px"}, "slow");
+    }*/
